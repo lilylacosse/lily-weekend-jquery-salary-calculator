1cda4668a677fb267b1d8937ad72b7751c8616cd
Lily LaCosse <lilylacosse@gmail.com>
2023-02-24 16:05:58 -0600

    can successfully add new employees to object array using submit button, next step is rendering

--- project/script.js
+++ project/script.js
@@ -2,4 +2,41 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log("JQ");
+
+  $("#submit-btn").on("click", submitEmployee);
+}
+let allEmployees = [];
+
+function emptyTable() {
+  console.log("In emptyTable()");
+  $("#original-message").empty();
+}
+
+function submitEmployee() {
+  console.log("In submitEmployee()");
+  // empty original form data
+  // i.e. "no employees yet!"
+  emptyTable();
+  // gather form data
+  const addFirstName = $("#first-name").val();
+  const addLastName = $("#last-name").val();
+  const addID = $("#id").val();
+  const addTitle = $("#title").val();
+  const addAnnualSalary = $("#annual-salary").val();
+
+  let newEmployee = {
+    firstName: addFirstName,
+    lastName: addLastName,
+    id: addID,
+    title: addTitle,
+    annualSalary: addAnnualSalary,
+  };
+  console.log("newEmployee:", newEmployee);
+
+  allEmployees.push(newEmployee);
+}
+
+function emptyTable() {
+  console.log("In emptyTable()");
+  $("#original-message").empty();
 }
