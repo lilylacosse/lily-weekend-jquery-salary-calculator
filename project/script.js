2ac2a48c3352819f572ffaea6cf7d1568225a129
Lily LaCosse <lilylacosse@gmail.com>
2023-02-25 11:24:10 -0600

    added resetInputs(), now to work on storing infor to calc monthly costs

--- project/script.js
+++ project/script.js
@@ -5,26 +5,43 @@ function onReady() {
 
   $("#submit-btn").on("click", submitEmployee);
 }
+
 let allEmployees = [];
 
+function resetInputs() {
+  console.log("START resetInputs");
+  $("input").val("");
+  //   $("#first-name").val("");
+  //   $("#last-name").val("");
+  //   $("#id").val("");
+  //   $("#title").val("");
+  //   $("#annual-salary").val("");
+  console.log("END resetInputs");
+}
+
 function emptyTable() {
-  console.log("In emptyTable()");
+  console.log("START emptyTable()");
   $(".tr").remove();
+  console.log("END emptyTable()");
 }
 
 function render() {
   console.log("In render()");
   emptyTable();
+
   // we are trying to render an array, so we have to loop through it
   for (let employee of allEmployees) {
+    console.log("employee", employee);
     $("#data-table").append(`<tr class="tr">
         <td>${employee.firstName}</td>
         <td>${employee.lastName}</td>
         <td>${employee.id}</td>
         <td>${employee.title}</td>
         <td>${employee.annualSalary}</td>
+        <td><button id="delete-btn">Delete</button></td>
         </tr>`);
   }
+  console.log("END render()");
 }
 
 function submitEmployee() {
@@ -50,7 +67,10 @@ function submitEmployee() {
     allEmployees.push(newEmployee);
     console.log("allEmployees update:", allEmployees);
 
+    resetInputs();
+
     render();
+    //resetInputs();
   } else {
     alert("Please Enter Employee Data");
   }
