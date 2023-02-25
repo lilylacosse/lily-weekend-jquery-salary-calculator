41e1bb6f3b41323d87af1b3b48681a9e57104623
Lily LaCosse <lilylacosse@gmail.com>
2023-02-25 16:53:12 -0600

    began subtractMonthlyCosts(), it is inputing the right values, need to check the actual subtraction and rendering

--- project/script.js
+++ project/script.js
@@ -4,9 +4,13 @@ function onReady() {
   console.log("JQ");
 
   $("#submit-btn").on("click", submitEmployee);
+
+  $("#data-table").on("click", "#delete-btn", deleteEmployee);
+  $("#data-table").on("click", "#delete-btn", subtractMonthlyCosts);
 }
 
 let allEmployees = [];
+let totalMonthlyPayroll = 0;
 
 function submitEmployee() {
   console.log("START OF submitEmployee()");
@@ -57,7 +61,7 @@ function resetInputs() {
 
 function calcMonthlyCosts() {
   console.log("START calcMonthlyCosts ");
-  let totalMonthlyPayroll = 0;
+  totalMonthlyPayroll = 0;
   for (let employee of allEmployees) {
     totalMonthlyPayroll += Number(employee.annualSalary) / 12;
   }
@@ -67,6 +71,7 @@ function calcMonthlyCosts() {
     $("#h2-total-payroll").css({ "background-color": "red" });
   }
   console.log("END calcMonthlyCosts ");
+  return totalMonthlyPayroll;
 }
 
 function render() {
@@ -79,7 +84,7 @@ function render() {
     $("#data-table").append(`<tr class="tr">
         <td>${employee.firstName}</td>
         <td>${employee.lastName}</td>
-        <td>${employee.id}</td>
+        <td id = "3">${employee.id}</td>
         <td>${employee.title}</td>
         <td>${employee.annualSalary}</td>
         <td><button id="delete-btn">Delete</button></td>
@@ -92,4 +97,52 @@ function emptyTable() {
   console.log("START emptyTable()");
   $(".tr").remove();
   console.log("END emptyTable()");
+}
+
+function deleteEmployee() {
+  console.log("START deleteEmployee():");
+
+  employeeToDelete = $(this).closest("tr");
+  console.log(employeeToDelete);
+  valueMatchID = employeeToDelete.children().eq(2).text();
+
+  console.log("Employee to Delete:", valueMatchID);
+
+  employeeToDelete.remove();
+
+  let newAllEmployees = [];
+
+  for (let employee of allEmployees) {
+    if (valueMatchID !== employee.id) {
+      newAllEmployees.push(employee);
+    }
+  }
+  allEmployees = newAllEmployees;
+  console.log(allEmployees);
+
+  render();
+  console.log("END deleteEmployee()");
+
+  return valueMatchID;
+}
+
+function subtractMonthlyCosts() {
+  console.log("START subtractMonthlyCosts() ");
+
+  for (let employee of allEmployees) {
+    console.log("employee id:", employee.id);
+    console.log("valueMatchId:", valueMatchID);
+    if (valueMatchID === employee.id) {
+      console.log("See YA Sucker");
+      totalMonthlyPayroll -= Number(employee.annualSalary) / 12;
+    }
+  }
+  console.log(totalMonthlyPayroll);
+
+  $("#total-monthly-payroll").html(`$${totalMonthlyPayroll}`);
+
+  if (totalMonthlyPayroll < 20000) {
+    $("#h2-total-payroll").css({ "background-color": "white" });
+  }
+  console.log("END subtractMonthlyCosts ");
 }
